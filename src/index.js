8b1f06d9e8956376080a3353637b5c6758130c44
GoncaloDuarte98 <up201605684@fe.up.pt>
2021-02-04 15:52:54 +0000

    meta tags update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import MetaTags from "react-meta-tags";
 
-
-document.title = "Goncalo Duarte";
+//document.title = "Goncalo Duarte";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MetaTags>
+      <title>Gonçalo Duarte</title>
+      <meta name="description" content="Gonçalo Duarte personal portfolio" />
+      <meta property="og:title" content="Gonçalo Duarte" />
+    </MetaTags>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
